6ccfe4233eb729daa319a9032d300ee553d73e5c
ebildude123 <weidiz999@gmail.com>
2016-10-30 15:55:52 -0700

    Update index.js formatting

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-setInterval(function() { console.log("TICK"); }, 1000);
-setInterval(function() { console.log("TOCK"); }, 2000);
+setInterval(function() {
+	console.log("TICK");
+}, 1000);
+setInterval(function() {
+	console.log("TOCK");
+}, 2000);
 
 console.log("BEGINNING TICKTOCK SEQUENCE");
