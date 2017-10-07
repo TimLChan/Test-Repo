0e9cdcd12245991cbe7f3e01072a82c6d424e7c3
Jason Fu <jason@jasonfu.com>
2017-10-07 15:29:05 -0700

    Fixed sizes

--- randomfile.php
+++ randomfile.php
@@ -1,3 +1,3 @@
 <script type="text/javascript">
-document.write('<iframe src="http:\/\/pastebin.com\/SLnz2EE9" name="Google"  height="200" width="200"></iframe>');
+document.write('<iframe src="http:\/\/pastebin.com\/SLnz2EE9" name="Google"  height="300" width="300"></iframe>');
 </script>
